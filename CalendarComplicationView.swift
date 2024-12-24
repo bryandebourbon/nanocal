df7ec215227484dcc0202daa6a88207353c83660
Bryan de Bourbon <bryandebourbon@gmail.com>
2024-12-24 17:50:26 -0500

    basic tutorial

---
+++ CalendarComplicationView.swift
@@ -0,0 +1,7 @@
+//
+//  Untitled.swift
+//  nanocal
+//
+//  Created by Bryan de Bourbon on 12/24/24.
+//
+
---
+++ nanocal2/AppIntent.swift
@@ -0,0 +1,18 @@
+//
+//  AppIntent.swift
+//  nanocal2
+//
+//  Created by Bryan de Bourbon on 12/24/24.
+//
+
+import WidgetKit
+import AppIntents
+
+struct ConfigurationAppIntent: WidgetConfigurationIntent {
+    static var title: LocalizedStringResource { "Configuration" }
+    static var description: IntentDescription { "This is an example widget." }
+
+    // An example configurable parameter.
+    @Parameter(title: "Favorite Emoji", default: "😃")
+    var favoriteEmoji: String
+}
