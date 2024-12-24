50158d921e7fc98f295f92bbc30cba233ad9067e
Bryan de Bourbon <bryandebourbon@gmail.com>
2024-12-24 15:14:01 -0500

    Initial Commit

---
+++ nanocal Watch App/nanocalApp.swift
@@ -0,0 +1,17 @@
+//
+//  nanocalApp.swift
+//  nanocal Watch App
+//
+//  Created by Bryan de Bourbon on 12/24/24.
+//
+
+import SwiftUI
+
+@main
+struct nanocal_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ nanocal/nanocalApp.swift
@@ -0,0 +1,17 @@
+//
+//  nanocalApp.swift
+//  nanocal
+//
+//  Created by Bryan de Bourbon on 12/24/24.
+//
+
+import SwiftUI
+
+@main
+struct nanocalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
